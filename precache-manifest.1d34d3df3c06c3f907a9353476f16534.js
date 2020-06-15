256b72e2ede442f4123804333c83a3f078804780
Benard <benardcharumbira@gmail.com>
2020-06-15 19:39:52 +0200

    Updates

---
+++ precache-manifest.1d34d3df3c06c3f907a9353476f16534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5428450aa7410a1236bfd46b420f7b",
+    "url": "/covi-stat/index.html"
+  },
+  {
+    "revision": "af4764ddfb6a2caad53c",
+    "url": "/covi-stat/static/css/main.4e438374.chunk.css"
+  },
+  {
+    "revision": "392942802811dd9457be",
+    "url": "/covi-stat/static/js/2.132ab4a1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covi-stat/static/js/2.132ab4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4764ddfb6a2caad53c",
+    "url": "/covi-stat/static/js/main.c7729ab3.chunk.js"
+  },
+  {
+    "revision": "1e4ddb81bedf2ea2cae3",
+    "url": "/covi-stat/static/js/runtime-main.05758258.js"
+  }
+]);
